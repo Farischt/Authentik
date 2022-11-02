6314ef61cb68a085ac985d9162b72fb40ed75ce7
Faris CHTATOU <faris.manage@gmail.com>
2022-11-03 00:09:12 +0100

    add new enum and type

--- src/auth/types/index.ts
+++ src/auth/types/index.ts
@@ -1,13 +1,31 @@
-import { User } from "@prisma/client"
+import { User, SessionToken } from "@prisma/client"
+import { UserWithoutPassword } from "../../user/types"
 
 export enum AuthError {
   EmailRequired = "Email is required !",
   EmailUnavailable = "Email is unavailable !",
+  EmailUnknown = "Email is unknown !",
   FirstNameRequired = "First name is required !",
+  InvalidCredentials = "Invalid credentials !",
   LastNameRequired = "Last name is required !",
   PasswordRequired = "Password is required !",
   PasswordNotLong = "Password must be at least 8 characters long !",
   PasswordNotStrong = "Password must be alphanumeric and contains a special character !",
+  Unknown = "Something went wrong !",
+}
+
+export enum AuthTokenError {
+  InvalidToken = "Invalid token !",
+  TokenExpired = "Token expired !",
 }
 
 export type LoginDto = Pick<User, "email" | "password">
+
+export type SessionDataFromCache = {
+  token: SessionToken
+  user: UserWithoutPassword
+}
+
+export type SessionTokenWithoutUserPassword = Omit<SessionToken, "user"> & {
+  user: UserWithoutPassword
+}
