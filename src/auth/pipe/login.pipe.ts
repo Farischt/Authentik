ec88806f5a65690f2b2074817e74e87a99a1132c
Faris CHTATOU <faris.manage@gmail.com>
2022-11-03 00:07:58 +0100

    add new validation pipe

---
+++ src/auth/pipe/login.pipe.ts
@@ -0,0 +1,24 @@
+import { PipeTransform, Injectable, BadRequestException } from "@nestjs/common"
+import { LoginDto } from "../types"
+import { AuthService } from "../service/auth.service"
+import { AuthError } from "../types"
+
+@Injectable()
+export class LoginPipe implements PipeTransform {
+  constructor(private readonly authService: AuthService) {}
+
+  async transform(input: LoginDto) {
+    const { email, password } = input
+    if (!email || typeof email !== "string") {
+      throw new BadRequestException(AuthError.EmailRequired)
+    } else if (!password || typeof password !== "string") {
+      throw new BadRequestException(AuthError.PasswordRequired)
+    } else if (!this.authService.isPasswordLongEnough(password)) {
+      throw new BadRequestException(AuthError.PasswordNotLong)
+    } else if (!this.authService.isPasswordStrong(password)) {
+      throw new BadRequestException(AuthError.PasswordNotStrong)
+    }
+
+    return input
+  }
+}
