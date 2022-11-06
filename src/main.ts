93a99edd754ae05e9253529d703d3b599c6ade86
Faris CHTATOU <faris.manage@gmail.com>
2022-11-06 01:15:53 +0100

    enable prisma disconnect

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as cookieParser from "cookie-parser"
 import { AppModule } from "./app/app.module"
 import { ConfigService } from "@nestjs/config"
 import { LoggingInterceptor } from "./app/interceptors/logging.interceptor"
+import { PrismaService } from "./database/prisma.service"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -11,5 +12,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new LoggingInterceptor())
   app.use(cookieParser())
   await app.listen(app.get(ConfigService).get("PORT") || 3000)
+  const prismaService = app.get(PrismaService)
+  await prismaService.enableShutdownHooks(app)
 }
 bootstrap()
