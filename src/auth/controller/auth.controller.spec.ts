0747f7faedca2d8fd1defb4253cc2c0d2ad5ea5c
Faris CHTATOU <faris.manage@gmail.com>
2022-11-06 01:16:13 +0100

    update testing module imports

--- src/auth/controller/auth.controller.spec.ts
+++ src/auth/controller/auth.controller.spec.ts
@@ -2,23 +2,18 @@ import { Test, TestingModule } from "@nestjs/testing"
 import { RedisModule } from "../../cache/redis.module"
 import { AuthController } from "./auth.controller"
 import { PrismaModule } from "../../database/prisma.module"
-import { UserModule } from "../../user/user.module"
-import { AuthModule } from "../../auth/auth.module"
+import { AuthService } from "../../auth/service/auth.service"
 import { ConfigurationModule as ConfigModule } from "../../config/config.module"
+import { UserService } from "../../user/service/user.service"
 
 describe("AuthController", () => {
   let controller: AuthController
 
   beforeEach(async () => {
-    // TODO: use valid dependencies
     const app: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule,
-        RedisModule,
-        UserModule,
-        PrismaModule,
-        AuthModule,
-      ],
+      imports: [ConfigModule, RedisModule, PrismaModule],
+      providers: [AuthService, UserService],
+      controllers: [AuthController],
     }).compile()
 
     controller = app.get<AuthController>(AuthController)
@@ -27,4 +22,8 @@ describe("AuthController", () => {
   it("should be defined", () => {
     expect(controller).toBeDefined()
   })
+
+  // describe("Register", () => {
+  //   it("should ")
+  // })
 })
