c9ae9a69942e2af76e03bb6f0120948572445a82
Faris CHTATOU <faris.manage@gmail.com>
2022-11-07 21:20:04 +0100

    add all exception filter

---
+++ src/app/filters/exception.filter.ts
@@ -0,0 +1,46 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  ForbiddenException,
+} from "@nestjs/common"
+import { HttpAdapterHost } from "@nestjs/core"
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    // In certain situations `httpAdapter` might not be available in the
+    // constructor method, thus we should resolve it here.
+    console.error(exception)
+    const { httpAdapter } = this.httpAdapterHost
+
+    const ctx = host.switchToHttp()
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR
+
+    const error =
+      exception instanceof HttpException ? exception.name : "UnknownError"
+
+    const message =
+      exception instanceof HttpException
+        ? exception.message
+        : "Something went wrong"
+
+    const responseBody = {
+      statusCode: httpStatus,
+      error: error,
+      message: message,
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+      timestamp: new Date().toISOString(),
+    }
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import { NestFactory } from "@nestjs/core"
+import { HttpAdapterHost, NestFactory } from "@nestjs/core"
 import * as cookieParser from "cookie-parser"
 
 import { AppModule } from "./app/app.module"
 import { ConfigService } from "@nestjs/config"
 import { LoggingInterceptor } from "./app/interceptors/logging.interceptor"
 import { PrismaService } from "./database/prisma.service"
+import { AllExceptionsFilter } from "./app/filters/exception.filter"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix("")
   app.useGlobalInterceptors(new LoggingInterceptor())
   app.use(cookieParser())
+  app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)))
   await app.listen(app.get(ConfigService).get("PORT") || 3000)
   const prismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
