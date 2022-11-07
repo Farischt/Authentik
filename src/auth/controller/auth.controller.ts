02aa6d8b490f29121549d551c34eb810fe64a83d
Faris CHTATOU <faris.manage@gmail.com>
2022-11-07 21:20:17 +0100

    remove try catch

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -17,7 +17,7 @@ import {
 } from "@nestjs/common"
 import { Request, Response } from "express"
 
-import { CreateUserDto, UserWithoutPassword } from "../../user/types"
+import { CreateUserDto, SerializedUser } from "../../user/types"
 import { AuthGuard } from "../guard/auth.guard"
 import { RegistrationPipe } from "../pipe/registration.pipe"
 import { LoginPipe } from "../pipe/login.pipe"
@@ -32,25 +32,18 @@ export class AuthController {
   @Post("register")
   async register(
     @Body(RegistrationPipe) input: CreateUserDto
-  ): Promise<UserWithoutPassword> {
+  ): Promise<SerializedUser> {
     const { email, password, firstName, lastName } = input
+    const hashedPassword = await this.authService.hashPassword(password)
+    const user = await this.authService.createUser({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    })
+    await this.authService.createAccountConfirmationToken(user.id)
 
-    try {
-      const hashedPassword = await this.authService.hashPassword(password)
-      const user = await this.authService.createUser({
-        email,
-        password: hashedPassword,
-        firstName,
-        lastName,
-      })
-      await this.authService.createAccountConfirmationToken(user.id)
-
-      return user
-    } catch (error) {
-      if (error instanceof HttpException) throw error
-      console.log(error)
-      throw new InternalServerErrorException(AuthError.Unknown)
-    }
+    return user
   }
 
   @Patch("confirm-account/:token")
@@ -58,39 +51,34 @@ export class AuthController {
   async confirmAccount(
     @Param("token", ParseUUIDPipe) token: string
   ): Promise<AuthConfirmAcountResponseType> {
-    try {
-      const accountConfirmationToken =
-        await this.authService.getAccountConfirmationToken({ id: token })
+    const accountConfirmationToken =
+      await this.authService.getAccountConfirmationToken({ id: token })
 
-      if (!accountConfirmationToken)
-        throw new BadRequestException("Invalid account confirmation token !")
+    if (!accountConfirmationToken)
+      throw new BadRequestException("Invalid account confirmation token !")
 
-      /* Removed in order to avoid case when user didn't confirm account whiting token expiration time
+    /* Removed in order to avoid case when user didn't confirm account whiting token expiration time
       if (!this.authService.isTokenValid(accountConfirmationToken))
       throw new BadRequestException("Account confirmation token expired !") */
 
-      if (
-        this.authService.isUserVerified(
-          await this.authService.getUser({
-            id: accountConfirmationToken.userId,
-          })
-        )
+    if (
+      this.authService.isUserVerified(
+        await this.authService.getUser({
+          id: accountConfirmationToken.userId,
+        })
       )
-        throw new BadRequestException("Account already confirmed !")
+    )
+      throw new BadRequestException("Account already confirmed !")
 
-      /** This update the user and delete the token */
-      const user = await this.authService.confirmAccount(
-        accountConfirmationToken.userId
-      )
-      if (!user) throw new NotFoundException("Couldn't confirm account !")
+    /** This update the user and delete the token */
+    const user = await this.authService.confirmAccount(
+      accountConfirmationToken.userId
+    )
+    if (!user) throw new NotFoundException("Couldn't confirm account !")
 
-      return {
-        message: "Successfully confirmed your email ",
-        email: user.email,
-      }
-    } catch (error) {
-      if (error instanceof HttpException) throw error
-      throw new InternalServerErrorException(AuthError.Unknown)
+    return {
+      message: "Successfully confirmed your email ",
+      email: user.email,
     }
   }
 
@@ -100,46 +88,31 @@ export class AuthController {
     @Req() req: Request,
     @Res() res: Response
   ): Promise<void> {
-    try {
-      const { email, password } = input
-      // Check if user exists
-      const user = await this.authService.getUser({ email })
-      if (!user) throw new BadRequestException("Invalid credentials !")
-      else if (!this.authService.isUserVerified(user)) {
-        throw new BadRequestException("Invalid credentials !")
-      }
-
-      // Check if password is correct
-      if (!(await this.authService.comparePassword(password, user.password)))
-        throw new BadRequestException("Invalid credentials !")
-
-      // Create session token
-      const sessionToken = await this.authService.createSessionToken(
-        user.id,
-        req.ip
-      )
-      await this.authService.setSessionCookie(res, sessionToken, user)
-      res.status(201).json({ message: "Logged in", loggedIn: true })
-    } catch (error) {
-      console.log(error)
-      if (error instanceof HttpException) throw error
-      throw new InternalServerErrorException(AuthError.Unknown)
+    const { email, password } = input
+    // Check if user exists
+    const user = await this.authService.getUser({ email })
+    if (!user) throw new BadRequestException("Invalid credentials !")
+    else if (!this.authService.isUserVerified(user)) {
+      throw new BadRequestException("Invalid credentials !")
     }
+
+    // Check if password is correct
+    if (!(await this.authService.comparePassword(password, user.password)))
+      throw new BadRequestException("Invalid credentials !")
+
+    // Create session token
+    const sessionToken = await this.authService.createSessionToken(
+      user.id,
+      req.ip
+    )
+    await this.authService.setSessionCookie(res, sessionToken, user)
+    res.status(201).json({ message: "Logged in", loggedIn: true })
   }
 
   @Get("user")
-  async getAuthenticatedUser(
-    @Req() req: Request
-  ): Promise<UserWithoutPassword> {
-    try {
-      const user = await this.authService.getAuthenticatedUser(req)
-      if (!user)
-        throw new NotFoundException("Couldn't find authenticated user !")
-
-      return user
-    } catch (error) {
-      if (error instanceof HttpException) throw error
-      throw new InternalServerErrorException(AuthError.Unknown)
-    }
+  async getAuthenticatedUser(@Req() req: Request): Promise<SerializedUser> {
+    const user = await this.authService.getAuthenticatedUser(req)
+    if (!user) throw new NotFoundException("Couldn't find authenticated user !")
+    return user
   }
 }
