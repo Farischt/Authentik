706bb18fcb6b0b9cf35f7ba6552ab272bdb6aca0
Faris CHTATOU <faris.manage@gmail.com>
2022-12-20 21:51:28 +0100

    add logger

--- src/app/filters/exception.filter.ts
+++ src/app/filters/exception.filter.ts
@@ -4,16 +4,19 @@ import {
   ArgumentsHost,
   HttpException,
   HttpStatus,
+  Logger,
 } from "@nestjs/common"
 import { HttpAdapterHost } from "@nestjs/core"
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllExceptionsFilter.name)
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
   catch(exception: unknown, host: ArgumentsHost): void {
     // In certain situations `httpAdapter` might not be available in the
     // constructor method, thus we should resolve it here.
+    this.logger.error(exception)
     const { httpAdapter } = this.httpAdapterHost
 
     const ctx = host.switchToHttp()
